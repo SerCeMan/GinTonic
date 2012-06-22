5c82c09a61f682e82a182c8d68e61519cd367d98
jcoder123 <jcoder123@93e531cb-8bc5-43e9-a6eb-085851364379>
2012-06-22 14:40:30 +0000

    removed unused method getCurrentSelection and getEditorInputForActiveEditor.
    git-svn-id: svn://svn.code.sf.net/p/egap/code/trunk@54 93e531cb-8bc5-43e9-a6eb-085851364379

--- egap/src/egap/utils/EditorUtils.java
+++ egap/src/egap/utils/EditorUtils.java
@@ -1,15 +1,9 @@
 package egap.utils;
 
-import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionProvider;
-import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.texteditor.ITextEditor;
 
 public class EditorUtils {
 
@@ -18,29 +12,6 @@ public class EditorUtils {
 		IWorkbenchPage activePage = activeWorkbenchWindow.getActivePage();
 		IEditorPart editorPart = activePage.getActiveEditor();
 		return editorPart;
-	}
-
-	public static ITextSelection getCurrentSelection() {
-		IEditorPart part = getActiveEditor();
-		if (part instanceof ITextEditor) {
-			final ITextEditor editor = (ITextEditor) part;
-			ISelectionProvider selectionProvider = editor.getSelectionProvider();
-			ISelection sel = selectionProvider.getSelection();
-			if (sel instanceof TextSelection) {
-				ITextSelection textSel = (ITextSelection) sel;
-				return textSel;
-			}
-		}
-		return null;
-	}
-
-	public static IEditorInput getEditorInputForActiveEditor() {
-		IEditorPart part = getActiveEditor();
-		if (part instanceof ITextEditor) {
-			final ITextEditor editor = (ITextEditor) part;
-			return editor.getEditorInput();
-		}
-		return null;
 	}
 
 }
