9d3dda65ae06df5807553b16376c27f46a26a3fb
jcoder123 <jcoder123@93e531cb-8bc5-43e9-a6eb-085851364379>
2012-07-25 05:36:00 +0000

    changed icon
    git-svn-id: svn://svn.code.sf.net/p/egap/code/trunk@85 93e531cb-8bc5-43e9-a6eb-085851364379

--- egap/src/egap/quickfix/assisted_inject/ProposalCreateAssistedFactory.java
+++ egap/src/egap/quickfix/assisted_inject/ProposalCreateAssistedFactory.java
@@ -55,7 +55,7 @@ public class ProposalCreateAssistedFactory implements IJavaCompletionProposal {
 
 	@Override
 	public Image getImage() {
-		return Icons.egapDefaultIconSmall;
+		return Icons.egapIconCreate;
 	}
 
 	@Override
