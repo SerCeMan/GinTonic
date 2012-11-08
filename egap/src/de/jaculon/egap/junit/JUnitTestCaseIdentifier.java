cbd56acce105a876190ba9feef32903dad66766c
jcoder123 <jcoder123@93e531cb-8bc5-43e9-a6eb-085851364379>
2012-11-08 15:44:33 +0000

    implemented JUnitTestCaseIdentifier
    git-svn-id: svn://svn.code.sf.net/p/egap/code/trunk@162 93e531cb-8bc5-43e9-a6eb-085851364379

--- egap/src/de/jaculon/egap/junit/JUnitTestCaseIdentifier.java
+++ egap/src/de/jaculon/egap/junit/JUnitTestCaseIdentifier.java
@@ -1,10 +1,34 @@
 package de.jaculon.egap.junit;
 
+import java.util.List;
+
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IType;
 
 public class JUnitTestCaseIdentifier {
 
+	private List<String> testSuffixes;
+
+	public void setTestSuffixes(List<String> testSuffixes) {
+		this.testSuffixes = testSuffixes;
+	}
+
+	/**
+	 * Returns true if the given ICompilationUnit is a JUnit Testcase. A
+	 * ICompilationUnit is considered to be a JUnit Testcase if it ends with any
+	 * of the provided test suffixes (see {@link #setTestSuffixes(List)}).
+	 */
 	public boolean isTestCase(ICompilationUnit icompilationUnit) {
+
+		IType primaryType = icompilationUnit.findPrimaryType();
+		String primaryTypeName = primaryType.getElementName();
+
+		for (String suffix : testSuffixes) {
+			if (primaryTypeName.endsWith(suffix)) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
