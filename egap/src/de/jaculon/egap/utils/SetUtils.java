8c597d52ff34110b9a621c52dd6b50c95f0c82b2
jcoder123 <jcoder123@93e531cb-8bc5-43e9-a6eb-085851364379>
2012-12-21 14:04:35 +0000

    snapshot
    git-svn-id: svn://svn.code.sf.net/p/egap/code/trunk@206 93e531cb-8bc5-43e9-a6eb-085851364379

--- egap/src/de/jaculon/egap/utils/SetUtils.java
+++ egap/src/de/jaculon/egap/utils/SetUtils.java
@@ -4,10 +4,6 @@ import java.util.HashSet;
 
 public class SetUtils {
 
-	public static <E> HashSet<E> newHashSet() {
-		return new HashSet<E>();
-	}
-
 	public static <E> HashSet<E> newHashSet(E... elements) {
 		int capacity = elements.length;
 		HashSet<E> set = new HashSet<E>(capacity);
